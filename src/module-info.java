e6058105a98cdbabe10bb4961ad66f69b63ec348
Igor Silva <igor719@gmail.com>
2024-03-12 15:08:09 -0300

    Main View design

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,11 @@
 module workshopJavafxJdbc {
 	requires javafx.controls;
+
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
+ 
+	//no meu caso eu não tinha adicionado essa linha
+        opens gui to javafx.graphics, javafx.fxml;
+	
 }
